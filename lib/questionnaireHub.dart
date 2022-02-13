b4fef4dc78b58e084054c98e34986e5ee45d3f19
c1943625 <ilyaso@cardiff.ac.uk>
2022-02-13 22:42:20 +0000

    Made a progress tracker for the questionnaire, but the pie chart makes too much white space in the table, so I won't be using it anymore.

--- lib/questionnaireHub.dart
+++ lib/questionnaireHub.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
+import 'package:syncfusion_flutter_charts/sparkcharts.dart';
 
 class QuestionnaireHub extends StatefulWidget {
   const QuestionnaireHub({Key? key}) : super(key: key);
@@ -11,10 +13,90 @@ class QuestionnaireHub extends StatefulWidget {
 class _QuestionnaireHubState extends State<QuestionnaireHub> {
   @override
   Widget build(BuildContext context) {
+    final List<ChartData> chartData = [
+      ChartData("F&S Yes", 2, Color.fromRGBO(0, 200, 0, 1)),
+      ChartData("F&S No", 1, Color.fromRGBO(200, 0, 0, 1)),
+    ];
     return Scaffold(
-        // appBar: AppBar(
-        //   title: Text("Hello"),
-        // ),
-        );
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: <Widget>[
+              const Padding(
+                padding: EdgeInsets.all(10.0),
+                child: Text(
+                  "Questionnaire Hub",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Table(
+                  children: [
+                    const TableRow(
+                      children: [
+                        Text(
+                          "Sections",
+                          textScaleFactor: 1.5,
+                        ),
+                        Text(
+                          "Progress",
+                          textScaleFactor: 1.5,
+                        ),
+                        Text(
+                          "Questionnaire Link",
+                          textScaleFactor: 1.5,
+                        ),
+                      ],
+                    ),
+                    TableRow(
+                      children: [
+                        const Text(
+                          "Fire & Safety",
+                          textScaleFactor: 1.5,
+                        ),
+                        Expanded(
+                          child: SfCircularChart(
+                            series: <CircularSeries>[
+                              // Renders doughnut chart
+                              DoughnutSeries<ChartData, String>(
+                                dataSource: chartData,
+                                pointColorMapper: (ChartData data, _) =>
+                                    data.color,
+                                xValueMapper: (ChartData data, _) => data.x,
+                                yValueMapper: (ChartData data, _) => data.y,
+                                dataLabelSettings:
+                                    const DataLabelSettings(isVisible: true),
+                                startAngle: 270, // Starting angle of doughnut
+                                endAngle: 90,
+                              ) // Ending angle of doughnut),
+                            ],
+                          ),
+                        ),
+                        const ElevatedButton(
+                          onPressed: null,
+                          child: const Text("Go to the Section?"),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
+}
+
+class ChartData {
+  ChartData(this.x, this.y, this.color);
+  final String x;
+  final double y;
+  final Color color;
 }
