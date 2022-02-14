0c2c3565a2eb9d7c6f57bb7ffdc140ee133c2087
Matt <mattdanielbarnett@gmail.com>
2022-02-14 10:27:11 +0000

    Added list of questions in a list view

--- lib/menu-help.dart
+++ lib/menu-help.dart
@@ -9,18 +9,100 @@ class MenuHelp extends StatefulWidget {
 
 class _MenuHelpState extends State<MenuHelp> {
 
+  static const double dividerThickness = 1;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+
       appBar: AppBar(
         backgroundColor: Colors.white,
         iconTheme: const IconThemeData(
           color: Colors.purple,
         ),
       ),
-      body: Center(
-          child: Text("Help")
-      ),
+
+      body: ListView(
+        padding: const EdgeInsets.all(8),
+        children: <Widget>[
+
+          Container(
+            height: 50,
+            child: const Center(child: Text('Question 1')),
+          ),
+
+          const Divider(
+            color: Colors.grey,
+            thickness: dividerThickness,
+          ),
+
+          Container(
+            height: 50,
+            child: const Center(child: Text('Question 2')),
+          ),
+
+          const Divider(
+            color: Colors.grey,
+            thickness: dividerThickness,
+          ),
+
+          Container(
+            height: 50,
+            child: const Center(child: Text('Question 3')),
+          ),
+
+          const Divider(
+            color: Colors.grey,
+            thickness: dividerThickness,
+          ),
+
+          Container(
+            height: 50,
+            child: const Center(child: Text('Question 4')),
+          ),
+
+          const Divider(
+            color: Colors.grey,
+            thickness: dividerThickness,
+          ),
+
+          Container(
+            height: 50,
+            child: const Center(child: Text('Question 5')),
+          ),
+
+          const Divider(
+            color: Colors.grey,
+            thickness: dividerThickness,
+          ),
+
+          Container(
+            height: 50,
+            child: const Center(child: Text('Question 6')),
+          ),
+
+          const Divider(
+            color: Colors.grey,
+            thickness: dividerThickness,
+          ),
+
+          Container(
+            height: 50,
+            child: const Center(child: Text('Question 7')),
+          ),
+
+          const Divider(
+            color: Colors.grey,
+            thickness: dividerThickness,
+          ),
+
+          Container(
+            height: 50,
+            child: const Center(child: Text('Question 8')),
+          ),
+
+        ],
+      )
     );
   }
 
