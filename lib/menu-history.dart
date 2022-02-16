b9a6240be9375a0acf4c2f0fac0be78edeb5dc18
Matt <mattdanielbarnett@gmail.com>
2022-02-16 15:53:46 +0000

    Added dynamic content and listtiles depending on record contents

--- lib/menu-history.dart
+++ lib/menu-history.dart
@@ -14,9 +14,25 @@ class _MenuHistoryState extends State<MenuHistory> {
 
   //Hard coded entries below -- to be removed
   void hardCodedRecords() {
+    records.clear();
     records.add(Record("[Example User 1]", DateTime.now(), "[Example 1]"));
     records.add(Record("[Example User 2]", DateTime.now(), "[Example 2]"));
     records.add(Record("[Example User 3]", DateTime.now(), "[Example 3]"));
+  }
+
+  List<ListTile> formatRecords() {
+    List<ListTile> recordListTiles = [];
+    for(var i = 0; i < records.length; i++) {
+      var currentRecord = records[i];
+      recordListTiles.add(ListTile(
+          title: Text("User " + currentRecord.user +
+              " added a response to section " +
+              currentRecord.section + " at " +
+              currentRecord.dateTime.toString() + "."),
+        )
+      );
+    }
+    return recordListTiles;
   }
 
   @override
@@ -32,11 +48,11 @@ class _MenuHistoryState extends State<MenuHistory> {
 
         body: ListView(
           padding: const EdgeInsets.all(8),
-          children: const <Widget>[
-
-          ]
+          children: formatRecords()
         )
     );
   }
+
+
 
 }
