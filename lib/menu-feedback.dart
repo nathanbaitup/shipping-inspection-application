553aa620ee224850a4519cbf1770c9304153862d
Matt <mattdanielbarnett@gmail.com>
2022-02-22 11:09:23 +0000

    Added email and name fields to feedback form, and submit button

--- lib/menu-feedback.dart
+++ lib/menu-feedback.dart
@@ -9,6 +9,8 @@ class MenuFeedback extends StatefulWidget {
 
 class _MenuFeedbackState extends State<MenuFeedback> {
 
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,8 +21,47 @@ class _MenuFeedbackState extends State<MenuFeedback> {
           ),
         ),
 
-        body: const Center(
-          child: Text("Placeholder")
+        body: Form (
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+
+              Text("1. Please enter your name. *"),
+
+              TextFormField(
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your name';
+                  }
+                  return null;
+                }
+              ),
+
+              Text("2. Please enter your email. *"),
+
+              TextFormField(
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your email';
+                  }
+                  return null;
+                }
+              ),
+
+              Center(
+                child: ElevatedButton(
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(content: Text('Processing Data')),
+                        );
+                      }
+                    }, child: const Text("Submit")
+                )
+              )
+            ],
+          )
         )
     );
   }
