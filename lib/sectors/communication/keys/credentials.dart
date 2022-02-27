035b0285d54d93b7da7508a2d89f8f9e0c385747
Hamid Iqbal <iqbalha@cardiff.ac.uk>
2022-02-27 12:19:49 +0000

    Token and appID stored within a seperate file for tidyness

---
+++ lib/sectors/communication/keys/credentials.dart
@@ -0,0 +1,6 @@
+const String appIDAgora = 'a186b891d7184e06be617cfab486944b';
+const String tokenAgora =
+    '006a186b891d7184e06be617cfab486944bIABcCEPXq+jZHAzK2y4GCSqv6LeXI+Jrt18pfLPYxscXSAx+f9gAAAAAEAATtvR9NIwbYgEAAQA0jBti';
+
+// TODO research more secure method to AppID and Token
+// Token must be changed every 24hr for developement purposes, video calling funtionaility will not work without a new key.
