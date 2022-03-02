77ba49eed7e9cc60a39d1139439636d32e89d917
Hamid Iqbal <iqbalha@cardiff.ac.uk>
2022-03-02 20:41:06 +0000

    credentials updated for developement branch

--- lib/sectors/communication/keys/credentials.dart
+++ lib/sectors/communication/keys/credentials.dart
@@ -1,6 +1,6 @@
 const String appIDAgora = 'a186b891d7184e06be617cfab486944b';
 const String tokenAgora =
-    '006a186b891d7184e06be617cfab486944bIADq1rbADD84tU5cJ9oIlZ197o+UhMRkSeJF55o6T4DSJQx+f9gAAAAAEAClV51H6w0eYgEAAQDrDR5i';
+    '006a186b891d7184e06be617cfab486944bIABsLNMNjtn9g6IcqQsp/yFlk4H9X00jIk4YsFjGqQ0Kgwx+f9gAAAAAEAAD1/lO0x8hYgEAAQDUHyFi';
 
 // TODO research more secure method to AppID and Token
 // Token must be changed every 24hr for developement purposes, video calling funtionaility will not work without a new key.
