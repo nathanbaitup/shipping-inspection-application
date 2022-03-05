2caf979092d44b71a209ccbfff13310b76cbf362
Matt <mattdanielbarnett@gmail.com>
2022-03-05 15:44:07 +0000

    Created new history type - 'call'

--- lib/sectors/drawer/drawer_history.dart
+++ lib/sectors/drawer/drawer_history.dart
@@ -37,6 +37,15 @@ class _MenuHistoryState extends State<MenuHistory> {
         }
         break;
 
+        case "call": {
+          currentRecordText[0] = currentRecord.user;
+          currentRecordText[1] = " joined a call channel called ";
+          currentRecordText[2] = currentRecord.section;
+          currentRecordText[3] = " at ";
+          currentRecordText[4] = DateFormat('kk:mm (yyyy-MM-dd)').format(currentRecord.dateTime);
+        }
+        break;
+
         default: {
           currentRecordText[0] = "NULL RECORD";
         }
