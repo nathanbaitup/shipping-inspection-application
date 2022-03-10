0052bf8f8f99e73c2176888cb3f15587763083fb
Hamid Iqbal <iqbalha@cardiff.ac.uk>
2022-03-10 16:31:17 +0000

    loading functionaility applied to QR_scanner_controller.

--- lib/utils/qr_scanner_controller.dart
+++ lib/utils/qr_scanner_controller.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:qr_code_scanner/qr_code_scanner.dart';
+import 'package:shipping_inspection_app/shared/loading.dart';
 
 import '../sectors/ar/ar_hub.dart';
 import 'colours.dart';
@@ -24,6 +25,9 @@ class _QRScannerState extends State<QRScanner> {
   Barcode? _qrResult;
   // The controller to access the QRView.
   QRViewController? _qrController;
+
+  // Loading bool statment
+  bool loading = false;
 
   // In order to get hot reload to work we need to pause the camera if the platform
   // is android, or resume the camera if the platform is iOS.
@@ -52,50 +56,60 @@ class _QRScannerState extends State<QRScanner> {
   // Styling for the QR Scanner Page.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSwatch().copyWith(
-          primary: LightColors.sPurple,
-          secondary: LightColors.sPurple,
-        ),
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text("Surveyor Camera"),
-        ),
-        body: Column(
-          children: <Widget>[
-            Expanded(
-              flex: 5,
-              child: QRView(
-                key: qrKey,
-                onQRViewCreated: _onQRViewCreated,
-                overlay: QrScannerOverlayShape(
-                  borderColor: LightColors.sPurple,
-                  borderWidth: 10.0,
-                  borderRadius: 10.0,
-                  overlayColor: const Color.fromRGBO(0, 0, 0, 95),
-                ),
+    return loading
+        ? const Loading()
+        : MaterialApp(
+            theme: ThemeData(
+              colorScheme: ColorScheme.fromSwatch().copyWith(
+                primary: LightColors.sPurple,
+                secondary: LightColors.sPurple,
               ),
             ),
-          ],
-        ),
-      ),
-    );
+            home: Scaffold(
+              appBar: AppBar(
+                title: const Text("Surveyor Camera"),
+              ),
+              body: Column(
+                children: <Widget>[
+                  Expanded(
+                    flex: 5,
+                    child: QRView(
+                      key: qrKey,
+                      onQRViewCreated: _onQRViewCreated,
+                      overlay: QrScannerOverlayShape(
+                        borderColor: LightColors.sPurple,
+                        borderWidth: 10.0,
+                        borderRadius: 10.0,
+                        overlayColor: const Color.fromRGBO(0, 0, 0, 95),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
   }
 
   // Checks if the QR codes scanned are useful for use within the application,
   // and navigates to the page of the AR section to survey.
   _launchARSection(String qrResult) async {
+    setState(() {
+      loading = true;
+      print('loading animation triggered TRUE, _performChannlNameConnection');
+    });
+
     if (_qrResult?.code == "f&s" ||
         _qrResult?.code == "engine" ||
         _qrResult?.code == "lifesaving") {
       Navigator.pop(context);
+      loading = false;
       await Navigator.push(
         context,
         MaterialPageRoute(
-          builder: (context) =>
-              ArHub(questionID: '${_qrResult?.code}', arContent: [" "], openThroughQR: true),
+          builder: (context) => ArHub(
+              questionID: '${_qrResult?.code}',
+              arContent: [" "],
+              openThroughQR: true),
         ),
       );
     } else {
