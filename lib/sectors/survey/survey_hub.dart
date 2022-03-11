032752c5637b4c3baf97277cdb20394d2a1ea8b5
Matt <mattdanielbarnett@gmail.com>
2022-03-11 02:05:22 +0000

    Added survey row widget with improved styling

--- lib/sectors/survey/survey_hub.dart
+++ lib/sectors/survey/survey_hub.dart
@@ -19,15 +19,6 @@ class SurveyHub extends StatefulWidget {
 }
 
 class _SurveyHubState extends State<SurveyHub> {
-  // Takes the user to the required survey section when pressing on an active survey.
-  void loadQuestion(String questionID) {
-    Navigator.push(
-      context,
-      MaterialPageRoute(
-        builder: (context) => SurveySection(questionID: questionID),
-      ),
-    );
-  }
 
   // Checks if camera permissions have been granted and takes the user to the QR
   // camera, updating the history page to allow for tracking.
@@ -53,227 +44,282 @@ class _SurveyHubState extends State<SurveyHub> {
     double screenWidth = MediaQuery.of(context).size.width;
     return Scaffold(
       resizeToAvoidBottomInset : false,
-      body: SafeArea(
-        child: Center(
-          child: Column(
-            children: <Widget>[
-              Container(
-                  height: 100,
-                  width: screenWidth,
-                  padding: const EdgeInsets.all(0.0),
-                  decoration: const BoxDecoration(
-                      color: LightColors.sPurple,
-                      borderRadius: BorderRadius.only(
-                        bottomRight: Radius.circular(30.0),
-                        bottomLeft: Radius.circular(30.0),
-                      )),
-                  child: const Center(
-                    child: Text(
-                      "AR Hub",
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 28,
-                        fontWeight: FontWeight.w500,
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Center(
+            child: Column(
+              children: <Widget>[
+                Container(
+                    height: 100,
+                    width: screenWidth,
+                    padding: const EdgeInsets.all(0.0),
+                    decoration: const BoxDecoration(
+                        color: LightColors.sPurple,
+                        borderRadius: BorderRadius.only(
+                          bottomRight: Radius.circular(30.0),
+                          bottomLeft: Radius.circular(30.0),
+                        )),
+                    child: const Center(
+                      child: Text(
+                        "AR Hub",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 28,
+                          fontWeight: FontWeight.w500,
+                        ),
                       ),
-                    ),
-                  )
-              ),
+                    )
+                ),
 
-              Container(
-                padding: const EdgeInsets.all(20.0),
-                child: Row(
-                  children: [
-                    Container(
-                      padding: const EdgeInsets.all(10.0),
-                      decoration: const BoxDecoration(
+                Container(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Row(
+                    children: [
+                      Container(
+                        padding: const EdgeInsets.all(10.0),
+                        decoration: const BoxDecoration(
+                            color: LightColors.sPurple,
+                            borderRadius: BorderRadius.all(Radius.circular(20)),
+                        ),
+                        child: const Text(
+                          "QR Camera",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 18,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ),
+
+                      RawMaterialButton(
+                        fillColor: LightColors.sDarkYellow,
+                        elevation: 2,
+                        shape: const CircleBorder(),
+                        child: const Text(
+                          "?",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 14,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                        onPressed: () {  },
+                      ),
+
+                      const Spacer(),
+
+                      SizedBox(
+                        width: screenWidth * 0.35,
+                        child: TextButton(
+                          style: TextButton.styleFrom(
+                            primary: Colors.white,
+                            backgroundColor: LightColors.sPurpleLL,
+                            elevation: 2,
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(18.0)),
+                          ),
+                          child: const Text(
+                            "Open QR Camera",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 14,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                          onPressed: () async => openCamera()
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+
+                const Divider(),
+
+                Container(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Row(
+                    children: [
+                      Container(
+                        padding: const EdgeInsets.all(10.0),
+                        decoration: const BoxDecoration(
                           color: LightColors.sPurple,
                           borderRadius: BorderRadius.all(Radius.circular(20)),
-                      ),
-                      child: const Text(
-                        "QR Camera",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 18,
-                          fontWeight: FontWeight.w500,
                         ),
-                      ),
-                    ),
-
-                    RawMaterialButton(
-                      fillColor: LightColors.sDarkYellow,
-                      elevation: 2,
-                      shape: const CircleBorder(),
-                      child: const Text(
-                        "?",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 14,
-                          fontWeight: FontWeight.w500,
-                        ),
-                      ),
-                      onPressed: () {  },
-                    ),
-
-                    const Spacer(),
-
-                    TextButton(
-                      style: TextButton.styleFrom(
-                        primary: Colors.white,
-                        backgroundColor: LightColors.sPurpleL,
-                        elevation: 2,
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(18.0)),
-                      ),
-                      child: const Text(
-                        "Open QR Camera",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 14,
-                          fontWeight: FontWeight.w500,
-                        ),
-                      ),
-                      onPressed: () async => openCamera()
-                    ),
-                  ],
-                ),
-              ),
-
-              const Divider(),
-
-              Container(
-                padding: const EdgeInsets.all(20.0),
-                child: Row(
-                  children: [
-                    Container(
-                      padding: const EdgeInsets.all(10.0),
-                      decoration: const BoxDecoration(
-                        color: LightColors.sPurple,
-                        borderRadius: BorderRadius.all(Radius.circular(20)),
-                      ),
-                      child: const Text(
-                        "Sections",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 18,
-                          fontWeight: FontWeight.w500,
-                        ),
-                      ),
-                    ),
-
-                    RawMaterialButton(
-                      fillColor: LightColors.sDarkYellow,
-                      elevation: 2,
-                      shape: const CircleBorder(),
-                      child: const Text(
-                        "?",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 14,
-                          fontWeight: FontWeight.w500,
-                        ),
-                      ),
-                      onPressed: () {  },
-                    ),
-
-                    Expanded(
-                      child: SizedBox(
-                        height: 35,
-                        child: TextFormField(
-                          decoration: InputDecoration(
-                            enabledBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(15),
-                              borderSide: const BorderSide(color: Colors.grey, width: 1.5),
-                            ),
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(15),
-                            ),
-                            prefixIcon: const Icon(
-                              Icons.search,
-                              size: 20,
-                            ),
-                            hintText: "Search",
-                            contentPadding: EdgeInsets.zero
+                        child: const Text(
+                          "Sections",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 18,
+                            fontWeight: FontWeight.w500,
                           ),
-                          style: const TextStyle(
-                            fontSize: 14,
-                          )
                         ),
-                      )
-                    )
-                  ],
-                ),
-              ),
+                      ),
 
-              Container(
-                padding: const EdgeInsets.only(
-                  top: 10,
-                  left: 20,
-                  right: 20,
+                      RawMaterialButton(
+                        fillColor: LightColors.sDarkYellow,
+                        elevation: 2,
+                        shape: const CircleBorder(),
+                        child: const Text(
+                          "?",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 14,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                        onPressed: () {  },
+                      ),
+
+                      Expanded(
+                        child: SizedBox(
+                          height: 35,
+                          child: TextFormField(
+                            decoration: InputDecoration(
+                              enabledBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(15),
+                                borderSide: const BorderSide(color: Colors.grey, width: 1.5),
+                              ),
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(15),
+                              ),
+                              prefixIcon: const Icon(
+                                Icons.search,
+                                size: 20,
+                              ),
+                              hintText: "Search",
+                              contentPadding: EdgeInsets.zero
+                            ),
+                            style: const TextStyle(
+                              fontSize: 14,
+                            )
+                          ),
+                        )
+                      )
+                    ],
+                  ),
                 ),
-                child: Table(
-                  children: [
-                    TableRow(
-                      children: [
-                        const Text(
-                          "Fire & Safety",
-                          textScaleFactor: 1,
-                        ),
-                        Text(
-                          "${questionBrain.getAnswerAmount("f&s")} of ${questionBrain.getQuestionAmount("f&s")}",
-                          textScaleFactor: 1,
-                        ),
-                        ElevatedButton(
-                          onPressed: () {
-                            loadQuestion('f&s');
-                          },
-                          child: const Text("Open"),
-                        ),
-                      ],
-                    ),
-                    TableRow(
-                      children: [
-                        const Text(
-                          "Lifesaving",
-                          textScaleFactor: 1,
-                        ),
-                        Text(
-                          "${questionBrain.getAnswerAmount("lifesaving")} of ${questionBrain.getQuestionAmount("lifesaving")}",
-                          textScaleFactor: 1,
-                        ),
-                        ElevatedButton(
-                          onPressed: () {
-                            loadQuestion('lifesaving');
-                          },
-                          child: const Text("Open"),
-                        ),
-                      ],
-                    ),
-                    TableRow(
-                      children: [
-                        const Text(
-                          "Engine Room",
-                          textScaleFactor: 1,
-                        ),
-                        Text(
-                          "${questionBrain.getAnswerAmount("engine")} of ${questionBrain.getQuestionAmount("engine")}",
-                          textScaleFactor: 1,
-                        ),
-                        ElevatedButton(
-                          onPressed: () {
-                            loadQuestion('engine');
-                          },
-                          child: const Text("Open"),
-                        ),
-                      ],
-                    ),
-                  ],
+
+                Container(
+                  padding: const EdgeInsets.only(
+                    top: 10,
+                    left: 20,
+                    right: 20,
+                  ),
+                  child: Column(
+                    children: const [
+                      SurveySectionWidget(
+                          sectionName: "Fire & Safety",
+                          sectionMethod: "f&s"),
+                      SurveySectionWidget(
+                          sectionName: "Lifesaving",
+                          sectionMethod: "lifesaving"),
+                      SurveySectionWidget(
+                          sectionName: "Engine Room",
+                          sectionMethod: "engine"),
+                    ],
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
-      ),
+      )
     );
   }
 }
+
+// Takes the user to the required survey section when pressing on an active survey.
+void loadQuestion(BuildContext context, String questionID) {
+  Navigator.push(
+    context,
+    MaterialPageRoute(
+      builder: (context) => SurveySection(questionID: questionID),
+    ),
+  );
+}
+
+class SurveySectionWidget extends StatelessWidget {
+  const SurveySectionWidget({Key? key,
+    required this.sectionName,
+    required this.sectionMethod}) : super(key: key);
+
+  final String sectionName;
+  final String sectionMethod;
+
+
+  @override
+  Widget build(BuildContext context) {
+    final double screenSize = MediaQuery.of(context).size.width;
+    return Row(
+      children: [
+        Container(
+          width: screenSize * 0.35,
+          padding: const EdgeInsets.only(
+            top: 20,
+            bottom: 20,
+          ),
+          margin: const EdgeInsets.only(
+            top: 5,
+            bottom: 5,
+          ),
+          decoration: const BoxDecoration(
+            color: LightColors.sPurpleL,
+            borderRadius: BorderRadius.all(Radius.circular(20)),
+          ),
+          child: Center(
+            child: Text(sectionName,
+            style: const TextStyle(
+              color: Colors.white,
+              fontSize: 16,
+              fontWeight: FontWeight.w500,
+              ),
+            ),
+          )
+        ),
+        Container(
+          width: screenSize * 0.2,
+          padding: const EdgeInsets.only(
+            top: 20,
+            bottom: 20,
+          ),
+          margin: const EdgeInsets.only(
+            right: 10,
+            left: 10,
+          ),
+          decoration: const BoxDecoration(
+            color: LightColors.sPurpleL,
+            borderRadius: BorderRadius.all(Radius.circular(20)),
+          ),
+          child: Center(
+            child: Text(
+            "${questionBrain.getAnswerAmount(sectionMethod)} of ${questionBrain.getQuestionAmount(sectionMethod)}",
+            style: const TextStyle(
+              color: Colors.white,
+              fontSize: 14,
+              ),
+            ),
+          )
+        ),
+        SizedBox(
+          width: screenSize * 0.3,
+          child: TextButton(
+            style: TextButton.styleFrom(
+              primary: Colors.white,
+              backgroundColor: LightColors.sPurpleLL,
+              elevation: 2,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20.0)),
+            ),
+            onPressed: () {
+              loadQuestion(context, sectionMethod);
+            },
+            child: const Text("Open"),
+          ),
+        )
+      ]
+    );
+  }
+
+}
+
