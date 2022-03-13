6ebf84d0575f598594726c1ae0d860e48bf61d97
Matt <mattdanielbarnett@gmail.com>
2022-03-13 01:30:44 +0000

    Added preference variables and switches

--- lib/sectors/drawer/settings/settings_history.dart
+++ lib/sectors/drawer/settings/settings_history.dart
@@ -3,6 +3,7 @@ import 'package:settings_ui/settings_ui.dart';
 import 'package:shipping_inspection_app/utils/colours.dart';
 
 import '../drawer_history.dart';
+import 'package:shipping_inspection_app/sectors/drawer/drawer_globals.dart' as globals;
 
 class SettingsHistory extends StatefulWidget {
   const SettingsHistory({Key? key}) : super(key: key);
@@ -67,39 +68,59 @@ class _SettingsHistoryState extends State<SettingsHistory> {
                   ),
                   tiles: [
                     SettingsTile.switchTile(
-                      title: const Text('Section Entering'),
-                      leading: const Icon(Icons.history,
+                      title: const Text("Section Entering"),
+                      leading: const Icon(Icons.door_back_door_outlined,
                           color: LightColors.sPurple),
-                      initialValue: null,
-                      onToggle: (bool value) {  },
+                      initialValue: globals.historyPrefs[0],
+                      activeSwitchColor: LightColors.sPurple,
+                      onToggle: (bool value) {
+                        globals.changeHistoryPref("Section Entering", !globals.historyPrefs[0]);
+                        setState(() { value = globals.historyPrefs[0]; });
+                      },
                     ),
                     SettingsTile.switchTile(
-                      title: const Text('Section Response'),
-                      leading: const Icon(Icons.history,
+                      title: const Text("Section Response"),
+                      leading: const Icon(Icons.newspaper,
                           color: LightColors.sPurple),
-                      initialValue: null,
-                      onToggle: (bool value) {  },
+                      initialValue: globals.historyPrefs[1],
+                      activeSwitchColor: LightColors.sPurple,
+                      onToggle: (bool value) {
+                        globals.changeHistoryPref("Section Response", !globals.historyPrefs[1]);
+                        setState(() { value = globals.historyPrefs[1]; });
+                      },
                     ),
                     SettingsTile.switchTile(
-                      title: const Text('Settings Change'),
-                      leading: const Icon(Icons.history,
+                      title: const Text("Settings Change"),
+                      leading: const Icon(Icons.settings,
                           color: LightColors.sPurple),
-                      initialValue: null,
-                      onToggle: (bool value) {  },
+                      initialValue: globals.historyPrefs[2],
+                      activeSwitchColor: LightColors.sPurple,
+                      onToggle: (bool value) {
+                        globals.changeHistoryPref("Settings Change", !globals.historyPrefs[2]);
+                        setState(() { value = globals.historyPrefs[2]; });
+                      },
                     ),
                     SettingsTile.switchTile(
-                      title: const Text('QR Usage'),
-                      leading: const Icon(Icons.history,
+                      title: const Text("QR Usage"),
+                      leading: const Icon(Icons.qr_code,
                           color: LightColors.sPurple),
-                      initialValue: null,
-                      onToggle: (bool value) {  },
+                      initialValue: globals.historyPrefs[3],
+                      activeSwitchColor: LightColors.sPurple,
+                      onToggle: (bool value) {
+                        globals.changeHistoryPref("QR Usage", !globals.historyPrefs[3]);
+                        setState(() { value = globals.historyPrefs[3]; });
+                      },
                     ),
                     SettingsTile.switchTile(
-                      title: const Text('Communications'),
-                      leading: const Icon(Icons.history,
+                      title: const Text("Communications"),
+                      leading: const Icon(Icons.phone,
                           color: LightColors.sPurple),
-                      initialValue: null,
-                      onToggle: (bool value) {  },
+                      initialValue: globals.historyPrefs[4],
+                      activeSwitchColor: LightColors.sPurple,
+                      onToggle: (bool value) {
+                        globals.changeHistoryPref("Communications", !globals.historyPrefs[4]);
+                        setState(() { value = globals.historyPrefs[4]; });
+                      },
                     ),
                   ],
                 ),
