54a0bc83c25b8ae1cf4e395e164e917702db7e9b
Matt <mattdanielbarnett@gmail.com>
2022-03-16 03:35:55 +0000

    Added save to file functionality

--- lib/sectors/drawer/drawer_globals.dart
+++ lib/sectors/drawer/drawer_globals.dart
@@ -1,16 +1,23 @@
 
 import 'package:shipping_inspection_app/sectors/history/record.dart';
+import 'package:path_provider/path_provider.dart';
+import 'dart:io';
 
-List<Record> records = [];
-String username = "Current User";
-
-List<bool> historyPrefs = List<bool>.filled(5, true, growable: false);
-
+// --- CALLS GLOBALS
+// -- For usage in Calls + Channels Settings
 List<String> savedChannels = List<String>.filled(3, " ", growable: false);
+
+// --- HISTORY GLOBALS
+// -- For usage in History Logs
+List<Record> records = [];
 
 void addRecord(type, user, dateTime, section) {
   records.add(Record(type, user, dateTime, section));
 }
+
+// --- USERNAME GLOBALS
+// -- For usage in Username Settings + History Logs + Calls
+String username = "Current User";
 
 void setUsername(newUsername) {
   username = newUsername;
@@ -19,6 +26,10 @@ void setUsername(newUsername) {
 String getUsername() {
   return username;
 }
+
+// --- HISTORY GLOBALS
+// -- For usage in History Logs + History Settings
+List<bool> historyPrefs = List<bool>.filled(5, true, growable: false);
 
 void changeHistoryPref(String type, bool value) {
   switch(type) {
@@ -47,4 +58,36 @@ void changeHistoryPref(String type, bool value) {
     }
     break;
   }
+}
+
+// --- LOCAL SAVING
+// For saving global variables locally for cross-session usage.
+
+
+// ---> REFERENCE START: https://programmingwithswift.com/how-to-save-a-file-locally-with-flutter/
+Future<String> getFilePath() async {
+  Directory appDocumentsDirectory = await getApplicationDocumentsDirectory(); // 1
+  String appDocumentsPath = appDocumentsDirectory.path; // 2
+  String filePath = '$appDocumentsPath/idwal_settings.txt'; // 3
+
+  return filePath;
+}
+// <--- REFERENCE END
+
+void saveFile() async {
+  File file = File(await getFilePath());
+  file.writeAsString(
+    username + "////" +
+    savedChannels.toString() + "////" +
+    historyPrefs.toString() + "////"
+  );
+
+  //CHECK THIS ---> https://stackoverflow.com/questions/43810508/dart-convert-string-representation-of-list-of-lists-to-list-of-list
+}
+
+void readFile() async {
+  File file = File(await getFilePath()); // 1
+  String fileContent = await file.readAsString(); // 2
+
+  print('File Content: $fileContent');
 }
