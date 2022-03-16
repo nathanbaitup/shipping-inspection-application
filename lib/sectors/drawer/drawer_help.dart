f6b127c20842900c15a38d0926636bf8b0cf3f64
c1943625 <ilyaso@cardiff.ac.uk>
2022-03-16 22:00:22 +0000

    Added the first question to the help page.

--- lib/sectors/drawer/drawer_help.dart
+++ lib/sectors/drawer/drawer_help.dart
@@ -12,66 +12,56 @@ class MenuHelp extends StatefulWidget {
 }
 
 class _MenuHelpState extends State<MenuHelp> {
-
   static const double dividerThickness = 1;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        iconTheme: const IconThemeData(
-          color: LightColors.sPurple,
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          iconTheme: const IconThemeData(
+            color: LightColors.sPurple,
+          ),
         ),
-      ),
-
-      body: ListView(
-        padding: const EdgeInsets.all(8),
-        children: const <Widget>[
-
-          QuestionWidget(
-              title: "Question 1",
+        body: ListView(
+          padding: const EdgeInsets.all(8),
+          children: const <Widget>[
+            QuestionWidget(
+              title: "What is the application about?",
               id: 1,
-          ),
-
-          Divider(
-            color: Colors.grey,
-            thickness: dividerThickness,
-          ),
-
-          QuestionWidget(
-            title: "Question 2",
-            id: 2,
-          ),
-
-          Divider(
-            color: Colors.grey,
-            thickness: dividerThickness,
-          ),
-
-          QuestionWidget(
-            title: "Question 3",
-            id: 3,
-          ),
-
-          Divider(
-            color: Colors.grey,
-            thickness: dividerThickness,
-          ),
-
-        ],
-      )
-    );
+            ),
+            Divider(
+              color: Colors.grey,
+              thickness: dividerThickness,
+            ),
+            QuestionWidget(
+              title: "Question 2",
+              id: 2,
+            ),
+            Divider(
+              color: Colors.grey,
+              thickness: dividerThickness,
+            ),
+            QuestionWidget(
+              title: "Question 3",
+              id: 3,
+            ),
+            Divider(
+              color: Colors.grey,
+              thickness: dividerThickness,
+            ),
+          ],
+        ));
   }
-
 }
 
 List questionContent(int id) {
-  switch(id) {
+  switch (id) {
     case 1:
-      content[0] = "Question 1 Answer";
-      content[1] = "Here is the answer to the question you selected (1).";
+      content[0] = "What is the application about?";
+      content[1] =
+          "This application is made so that the survey can answer their questions"
+          " and if they are stuck on any of the questions they can call the expert for help. ";
       break;
     case 2:
       content[0] = "Question 2 Answer";
@@ -86,7 +76,8 @@ List questionContent(int id) {
 }
 
 class QuestionWidget extends StatelessWidget {
-  const QuestionWidget({Key? key, required this.title, required this.id}) : super(key: key);
+  const QuestionWidget({Key? key, required this.title, required this.id})
+      : super(key: key);
 
   final String title;
   final int id;
@@ -94,24 +85,22 @@ class QuestionWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text(title),
-      onTap: () {
-        questionContent(id);
-        showDialog<String>(
-          context: context,
-          builder: (BuildContext context) =>
-              AlertDialog(
-                title: Text(content[0]),
-                content: Text(content[1]),
-                actions: <Widget>[
-                  TextButton(
-                    onPressed: () => Navigator.pop(context, 'Close'),
-                    child: const Text('Close'),
-                  ),
-                ],
-              ),
-        );
-      }
-    );
+        title: Text(title),
+        onTap: () {
+          questionContent(id);
+          showDialog<String>(
+            context: context,
+            builder: (BuildContext context) => AlertDialog(
+              title: Text(content[0]),
+              content: Text(content[1]),
+              actions: <Widget>[
+                TextButton(
+                  onPressed: () => Navigator.pop(context, 'Close'),
+                  child: const Text('Close'),
+                ),
+              ],
+            ),
+          );
+        });
   }
 }
