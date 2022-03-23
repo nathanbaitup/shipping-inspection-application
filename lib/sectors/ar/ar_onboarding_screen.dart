87f14c03f740fd22747e4bca0eb5683c72a92edb
Nathan Baitup <baitupnw@cardiff.ac.uk>
2022-03-23 23:36:04 +0000

    Added image assets for the onboarding screen and updated the onboarding screen to include instructions on how to use the AR section fully. Need to change the Duck Model to a useful model to update instruction images.

--- lib/sectors/ar/ar_onboarding_screen.dart
+++ lib/sectors/ar/ar_onboarding_screen.dart
@@ -50,7 +50,7 @@ class _ARIntroductionState extends State<ARIntroduction> {
   // REFERENCE accessed 23/03/2022 https://pub.dev/packages/onboarding
   // Used to create the next and finish buttons.
   void _buildButton(int pageIndex) {
-    if (pageIndex == 2) {
+    if (pageIndex == 5) {
       setState(() {
         materialButton = _finishButton;
       });
@@ -102,38 +102,251 @@ class _ARIntroductionState extends State<ARIntroduction> {
   // Used template to create on-boarding widget with instructions on how to use the AR scene.
   final onboardingPagesList = [
     PageModel(
-      widget: Column(
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.symmetric(
-              horizontal: 45.0,
-              vertical: 45.0,
+      widget: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 45.0,
+                vertical: 45.0,
+              ),
+              child: Image.asset('images/avatar.png'),
             ),
-            child: Image.asset('images/avatar.png'),
-          ),
-          Container(
-            padding: const EdgeInsets.symmetric(
-              horizontal: 45.0,
+            Container(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 45.0,
+              ),
+              alignment: Alignment.center,
+              child: const Text(
+                'Welcome to the AR Viewer.',
+                style: pageTitleStyle,
+              ),
             ),
-            alignment: Alignment.center,
-            child: const Text(
-              'Welcome to the AR Viewer.',
-              style: pageTitleStyle,
+            Container(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 45.0,
+                vertical: 15.0,
+              ),
+              alignment: Alignment.center,
+              child: const Text(
+                'This guide will give you all the information needed to interact with and use the AR viewer.',
+                style: pageInfoStyle,
+              ),
             ),
-          ),
-          Container(
-            padding: const EdgeInsets.symmetric(
-              horizontal: 45.0,
-              vertical: 15.0,
+          ],
+        ),
+      ),
+    ),
+    PageModel(
+      widget: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Container(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 45.0,
+                vertical: 45.0,
+              ),
+              alignment: Alignment.center,
+              child: const Text(
+                'Questions:',
+                style: pageTitleStyle,
+              ),
             ),
-            alignment: Alignment.center,
-            child: const Text(
-              'This guide will give you all the information needed to interact with and use the AR viewer.',
-              style: pageInfoStyle,
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 45.0,
+                vertical: 20.0,
+              ),
+              child: Image.asset('images/ar_onboarding/show_questions_1.png'),
             ),
-          ),
-        ],
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 45.0,
+                vertical: 20.0,
+              ),
+              child: Image.asset('images/ar_onboarding/show_questions_2.png'),
+            ),
+            Container(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 45.0,
+                vertical: 15.0,
+              ),
+              alignment: Alignment.center,
+              child: const Text(
+                'At the top of the screen, you will see the section your are inspecting and the relevant questions within the section. To cycle through each question, tap on the question text.',
+                style: pageInfoStyle,
+              ),
+            ),
+          ],
+        ),
+      ),
+    ),
+    PageModel(
+      widget: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Container(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 45.0,
+                vertical: 45.0,
+              ),
+              alignment: Alignment.center,
+              child: const Text(
+                'Move to view in scene indicator:',
+                style: pageTitleStyle,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 45.0,
+                vertical: 20.0,
+              ),
+              child:
+                  Image.asset('images/ar_onboarding/show_loading_animator.png'),
+            ),
+            Container(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 45.0,
+                vertical: 15.0,
+              ),
+              alignment: Alignment.center,
+              child: const Text(
+                'When loading into the AR view, you will see a floating hand holding a phone. This animation is to move the device to allow the camera to locate an area to display an AR image.',
+                style: pageInfoStyle,
+              ),
+            ),
+          ],
+        ),
+      ),
+    ),
+    PageModel(
+      widget: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Container(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 45.0,
+                vertical: 45.0,
+              ),
+              alignment: Alignment.center,
+              child: const Text(
+                'AR Controls:',
+                style: pageTitleStyle,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 45.0,
+                vertical: 20.0,
+              ),
+              child:
+                  Image.asset('images/ar_onboarding/show_option_controls.png'),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 45.0,
+                vertical: 20.0,
+              ),
+              child: Image.asset(
+                  'images/ar_onboarding/show_controls_with_screenshot.png'),
+            ),
+            Container(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 45.0,
+                vertical: 15.0,
+              ),
+              alignment: Alignment.center,
+              child: const Text(
+                'At the bottom of the screen are the controls within the AR scene. \n\nPressing on the camera button captures an image of the camera and AR model on screen and displays it in a preview at the bottom right hand side of the screen. \n\nThe tick saves any images taken within the AR view into the survey section. \n\nThe cross resets the AR scene back to an empty scene.',
+                style: pageInfoStyle,
+              ),
+            ),
+          ],
+        ),
+      ),
+    ),
+    PageModel(
+      widget: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Container(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 45.0,
+                vertical: 45.0,
+              ),
+              alignment: Alignment.center,
+              child: const Text(
+                'AR Scene Detector:',
+                style: pageTitleStyle,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 45.0,
+                vertical: 15.0,
+              ),
+              child:
+                  Image.asset('images/ar_onboarding/show_plane_detection.png'),
+            ),
+            Container(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 45.0,
+                vertical: 15.0,
+              ),
+              alignment: Alignment.center,
+              child: const Text(
+                'Once the camera has found a place to add an AR scene, the floating hand animation disappears and a texture of where a model can be placed is displayed on the screen. \n\nTo add a model to the screen, simply tap the area you would like the model to appear.',
+                style: pageInfoStyle,
+              ),
+            ),
+          ],
+        ),
+      ),
+    ),
+    PageModel(
+      widget: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Container(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 45.0,
+                vertical: 45.0,
+              ),
+              alignment: Alignment.center,
+              child: const Text(
+                'Adding and Viewing an Model:',
+                style: pageTitleStyle,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 45.0,
+                vertical: 15.0,
+              ),
+              child: Image.asset('images/ar_onboarding/show_model_1.png'),
+            ),
+            Container(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 45.0,
+                vertical: 15.0,
+              ),
+              alignment: Alignment.center,
+              child: const Text(
+                'Once a model has been added, you will see it on the screen. This model is interactable by being both movable and rotatable. \n\nTo view the model in more detail, simply move your device closer and around the model.',
+                style: pageInfoStyle,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 45.0,
+                vertical: 45.0,
+              ),
+              child: Image.asset('images/ar_onboarding/show_model_2.png'),
+            ),
+          ],
+        ),
       ),
     ),
   ];
+  // END REFERENCE
 }
