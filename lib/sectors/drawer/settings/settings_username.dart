29c8c375f847eae1c75bfbd94b0daa8d83843e87
c1943625 <ilyaso@cardiff.ac.uk>
2022-03-26 19:31:44 +0000

    Made it so when the user changes their user name to something new, that gets sent to the History_Logging firestore database.

--- lib/sectors/drawer/settings/settings_username.dart
+++ lib/sectors/drawer/settings/settings_username.dart
@@ -1,5 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:shipping_inspection_app/sectors/drawer/drawer_globals.dart' as globals;
+import 'package:shipping_inspection_app/sectors/drawer/drawer_globals.dart'
+    as globals;
 import 'package:shipping_inspection_app/utils/colours.dart';
 
 class SettingsUsername extends StatefulWidget {
@@ -10,7 +12,6 @@ class SettingsUsername extends StatefulWidget {
 }
 
 class _SettingsUsernameState extends State<SettingsUsername> {
-
   late String username;
 
   String currentUsername = globals.getUsername();
@@ -28,23 +29,20 @@ class _SettingsUsernameState extends State<SettingsUsername> {
     final Size screenSize = MediaQuery.of(context).size;
 
     return Scaffold(
-        resizeToAvoidBottomInset : false,
-
+        resizeToAvoidBottomInset: false,
         appBar: AppBar(
           backgroundColor: Colors.white,
           iconTheme: const IconThemeData(
             color: LightColors.sPurple,
           ),
         ),
-
         body: Container(
             padding: const EdgeInsets.all(20.0),
-            child: Form (
+            child: Form(
                 key: _formKey,
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-
                     Container(
                       padding: const EdgeInsets.only(top: 20.0),
                       child: Column(
@@ -53,7 +51,8 @@ class _SettingsUsernameState extends State<SettingsUsername> {
                             child: Column(
                               children: [
                                 const Text("Your username is currently: "),
-                                Text(currentUsername,
+                                Text(
+                                  currentUsername,
                                   style: const TextStyle(
                                     color: LightColors.sPurple,
                                     fontWeight: FontWeight.bold,
@@ -65,7 +64,6 @@ class _SettingsUsernameState extends State<SettingsUsername> {
                         ],
                       ),
                     ),
-
                     TextFormField(
                         validator: (value) {
                           if (value == null || value.isEmpty) {
@@ -76,35 +74,44 @@ class _SettingsUsernameState extends State<SettingsUsername> {
                           return null;
                         },
                         decoration: const InputDecoration(
-                            hintText: 'Sarah',
-                            labelText: 'Username'
-                        )
-                    ),
-
-
+                            hintText: 'Sarah', labelText: 'Username')),
                     Container(
                       width: screenSize.width,
                       child: ElevatedButton(
                         child: const Text('Submit'),
-                        onPressed: () {
+                        onPressed: () async {
                           if (_formKey.currentState!.validate()) {
                             ScaffoldMessenger.of(context).showSnackBar(
-                              const SnackBar(content: Text('Processing username change...')),
+                              const SnackBar(
+                                  content:
+                                      Text('Processing username change...')),
                             );
-                            globals.addRecord("settings-username-change", globals.getUsername(), DateTime.now(), username);
+                            globals.addRecord(
+                                "settings-username-change",
+                                globals.getUsername(),
+                                DateTime.now(),
+                                username);
                             globals.setUsername(username);
                             updateCurrentUsername();
                             globals.savePrefs();
+
+                            await FirebaseFirestore.instance
+                                .collection("History_Logging")
+                                .add({
+                                  'title': "Changing my username",
+                                  'username': globals.getUsername(),
+                                  'time': DateTime.now(),
+                                  'permission': "User Name",
+                                })
+                                .then((value) => print("Record has been added"))
+                                .catchError((error) =>
+                                    print("Failed to add record: $error"));
                           }
                         },
                       ),
                       margin: const EdgeInsets.only(top: 20.0),
                     )
                   ],
-                )
-            )
-        )
-    );
+                ))));
   }
-
 }
