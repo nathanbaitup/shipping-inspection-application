dcab02470eb37f0419f91eb32bbf440c3c0ca510
Brian Ekong <ekongbn@cardiff.ac.uk>
2022-03-30 18:59:20 +0100

    Updated dependencies and removed unused import

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import 'package:shipping_inspection_app/utils/colours.dart';
 import 'package:shipping_inspection_app/userdashboard.dart';
 import 'package:permission_handler/permission_handler.dart';
