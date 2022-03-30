45dfa1f6eed8442875d1fce99b85f47b43d6c641
Nathan Baitup <baitupnw@cardiff.ac.uk>
2022-03-30 22:09:29 +0100

    Updated main_test to run each test individually.

--- test/main_test.dart
+++ test/main_test.dart
@@ -3,44 +3,75 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:shipping_inspection_app/main.dart';
 
 void main() {
-  testWidgets("Test the main page displays the welcome page and its widgets.",
-      (WidgetTester tester) async {
-    // Calls the application to run main.
-    await tester.pumpWidget(const ShipApp());
+  group("Testing the main and welcome screens display correctly", () {
+    testWidgets("Call the main screen.", (WidgetTester tester) async {
+      await tester.pumpWidget(const ShipApp());
+    });
 
-    const String titleText = 'Idwal Vessel Inspector';
-    const String vesselIDErrorText = 'Please enter a Vessel name or ID';
-    const String homePageTitleText = 'Current User';
-    const String vesselID = 'vesselTestID';
+    testWidgets("Should find the first page to open be the Welcome Screen",
+        ((tester) async {
+      await tester.pumpWidget(const ShipApp());
 
-    // Should find the first page to open be the Welcome Screen.
-    var inputVesselField = find.byType(TextField);
-    expect(inputVesselField, findsOneWidget);
+      var inputVesselField = find.byType(TextField);
+      expect(inputVesselField, findsOneWidget);
+    }));
 
-    // Should find the title text.
-    var welcomeTitle = find.text(titleText);
-    expect(welcomeTitle, findsOneWidget);
+    testWidgets("Should find the welcome text", ((tester) async {
+      await tester.pumpWidget(const ShipApp());
 
-    // Should find the button to save an entered ID.
-    var submitButton = find.byType(ElevatedButton);
-    expect(submitButton, findsOneWidget);
+      const String titleText = 'Idwal Vessel Inspector';
+      var welcomeTitle = find.text(titleText);
+      expect(welcomeTitle, findsOneWidget);
+    }));
 
-    // Should press the button with no text entered.
-    // Should return the validation error text.
-    await tester.tap(submitButton);
-    debugPrint('button pressed');
-    await tester.pump();
-    expect(find.text(vesselIDErrorText), findsOneWidget);
+    testWidgets("Should find the button to save an ID", ((tester) async {
+      await tester.pumpWidget(const ShipApp());
 
-    // Tests that text can be entered in the text field.
-    await tester.enterText(inputVesselField, vesselID);
-    expect(find.text('vesselTestID'), findsOneWidget);
+      var submitButton = find.byType(ElevatedButton);
+      expect(submitButton, findsOneWidget);
+    }));
 
-    // Tests that when a button is pressed with a vessel ID, it returns to the home page
-    // Should display the default user and vesselID.
-    await tester.tap(submitButton);
-    await tester.pump();
-    expect(find.text(homePageTitleText), findsOneWidget);
-    expect(find.text('Vessel: $vesselID'), findsOneWidget);
+    testWidgets(
+        "Should press the button with no text entered, should return validation error",
+        ((tester) async {
+      await tester.pumpWidget(const ShipApp());
+
+      const String vesselIDErrorText = 'Please enter a Vessel name or ID';
+
+      var submitButton = find.byType(ElevatedButton);
+      expect(submitButton, findsOneWidget);
+
+      await tester.tap(submitButton);
+      debugPrint('button pressed');
+      await tester.pump();
+      expect(find.text(vesselIDErrorText), findsOneWidget);
+    }));
+
+    testWidgets("Should enter and display text in text field", ((tester) async {
+      await tester.pumpWidget(const ShipApp());
+
+      const String vesselID = 'vesselTestID';
+      var inputVesselField = find.byType(TextField);
+      await tester.enterText(inputVesselField, vesselID);
+      expect(find.text('vesselTestID'), findsOneWidget);
+    }));
+
+    testWidgets("Should submit vesselID and return the home page.",
+        (WidgetTester tester) async {
+      await tester.pumpWidget(const ShipApp());
+
+      const String homePageTitleText = 'Current User';
+      const String vesselID = 'vesselTestID';
+
+      var inputVesselField = find.byType(TextField);
+      await tester.enterText(inputVesselField, vesselID);
+
+      var submitButton = find.byType(ElevatedButton);
+      await tester.tap(submitButton);
+      await tester.pump();
+
+      expect(find.text(homePageTitleText), findsOneWidget);
+      expect(find.text('Vessel: $vesselID'), findsOneWidget);
+    });
   });
 }
